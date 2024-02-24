8092ed1ebe02ba91e2cceb4664dbfdff3cca5a99
kiladskj <159625525+kiladskj@users.noreply.github.com>
2024-02-24 14:17:24 +0300

    Add files via upload

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public int healt = 5;
+    public int level = 1;
+    public float speed = 1.2f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello, world!");
+        healt += level;
+        print("Çהמנמגüו:" + healt);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
     }
 }
