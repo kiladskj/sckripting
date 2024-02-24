ace0f0805c12a6f42ec8f2df3096961967bdfa2b
kiladskj <159625525+kiladskj@users.noreply.github.com>
2024-02-24 15:34:38 +0300

    Add files via upload

---
+++ Spid triger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeeTriger : MonoBehaviour
+{
+    public float speedFactor = 2.5f;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
